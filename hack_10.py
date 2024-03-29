fd560d42a59e0cd14add0dae7a5270d1a84361fe
Therelam204 <152944160+Therelam204@users.noreply.github.com>
2024-03-28 21:32:42 -0400

    Update hack_10.py

--- hack_10.py
+++ hack_10.py
@@ -6,7 +6,7 @@ def fn_hack_10():
     result = "fooziman"
 
     
-    result = result.replace("o","0" ,2)
+    result = result.replace("o","0")
     result = result.replace("i","1")
     result = result.replace("a","@")
     result = result.upper()
